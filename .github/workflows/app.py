b55bdefdd5a20770481f74127477eed4d6f44f8c
Aakash <ak3437541199@gmail.com>
2026-04-11 15:03:08 +0530

    lint 11

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,32 +1,12 @@
-from flask import Flask
-import psycopg2
-import redis
-import time
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-def check_db():
-    try:
-        conn = psycopg2.connect(
-            host="db",
-            database="testdb",
-            user="user",
-            password="password"
-        )
-        return "Database Connected"
-    except:
-        return "DB not ready"
 
-def check_cache():
-    try:
-        r = redis.Redis(host="redis", port=6379)
-        r.set("msg", "Hello from Redis")
-        return r.get("msg").decode()
-    except:
-        return "Redis not ready"
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return f"{check_db()} | {check_cache()}"
 
-app.run(host="0.0.0.0", port=5000)
+@app.route('/health')
+def health():
+    return 'Server is up and running'
