76f90c580643e4427a5a00b377eb907ec8d64359
Aakash <ak3437541199@gmail.com>
2026-04-14 00:48:02 +0530

    fix: lines issuw in app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -10,5 +9,6 @@ def hello_world():
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
 
 app.run(debug=True, host='0.0.0.0', port=80)
